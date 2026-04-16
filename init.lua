23d491b9ace7b14cc9cd7bc1fe6c4f20e04f4a2a
cookie1170 <171882521+cookie1170@users.noreply.github.com>
2026-04-16 17:57:38 +1000

    added multiple-cursors.nvim

--- init.lua
+++ init.lua
@@ -740,6 +740,44 @@ require('lazy').setup({
     opts = { signs = false },
   },
 
+  { -- Multiple cursors
+    'brenton-leighton/multiple-cursors.nvim',
+    opts = {
+      pre_hook = function() require('nvim-autopairs').disable() end,
+      post_hook = function() require('nvim-autopairs').enable() end,
+      custom_key_maps = {
+        { 'n', { '<A-k>', '<A-Up>' }, function() MiniMove.move_line 'up' end },
+        { 'n', { '<A-j>', '<A-Down>' }, function() MiniMove.move_line 'down' end },
+        { 'n', { '<A-h>', '<A-Left>' }, function() MiniMove.move_line 'left' end },
+        { 'n', { '<A-l>', '<A-Right>' }, function() MiniMove.move_line 'right' end },
+
+        { 'x', { '<A-k>', '<A-Up>' }, function() MiniMove.move_selection 'up' end },
+        { 'x', { '<A-j>', '<A-Down>' }, function() MiniMove.move_selection 'down' end },
+        { 'x', { '<A-h>', '<A-Left>' }, function() MiniMove.move_selection 'left' end },
+        { 'x', { '<A-l>', '<A-Right>' }, function() MiniMove.move_selection 'right' end },
+      },
+    },
+    keys = {
+      { '<C-j>', '<Cmd>MultipleCursorsAddDown<CR>', mode = { 'n', 'x' }, desc = 'Add cursor and move down' },
+      { '<C-k>', '<Cmd>MultipleCursorsAddUp<CR>', mode = { 'n', 'x' }, desc = 'Add cursor and move up' },
+
+      { '<C-Up>', '<Cmd>MultipleCursorsAddUp<CR>', mode = { 'n', 'i', 'x' }, desc = 'Add cursor and move up' },
+      { '<C-Down>', '<Cmd>MultipleCursorsAddDown<CR>', mode = { 'n', 'i', 'x' }, desc = 'Add cursor and move down' },
+
+      { '<C-LeftMouse>', '<Cmd>MultipleCursorsMouseAddDelete<CR>', mode = { 'n', 'i' }, desc = 'Add or remove cursor' },
+
+      { '<Leader>m', '<Cmd>MultipleCursorsAddVisualArea<CR>', mode = { 'x' }, desc = 'Add cursors to the lines of the visual area' },
+
+      { '<Leader>a', '<Cmd>MultipleCursorsAddMatches<CR>', mode = { 'n', 'x' }, desc = 'Add cursors to cword' },
+      { '<Leader>A', '<Cmd>MultipleCursorsAddMatchesV<CR>', mode = { 'n', 'x' }, desc = 'Add cursors to cword in previous area' },
+
+      { '<Leader>d', '<Cmd>MultipleCursorsAddJumpNextMatch<CR>', mode = { 'n', 'x' }, desc = 'Add cursor and jump to next cword' },
+      { '<Leader>D', '<Cmd>MultipleCursorsJumpNextMatch<CR>', mode = { 'n', 'x' }, desc = 'Jump to next cword' },
+
+      { '<Leader>l', '<Cmd>MultipleCursorsLock<CR>', mode = { 'n', 'x' }, desc = 'Lock virtual cursors' },
+    },
+  },
+
   { -- Use :terminal in a floating window
     'voldikss/vim-floaterm',
     keys = {
